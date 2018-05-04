da5a2c4747de3d1ee09f6722227d3ad2efbdf56e
So-Xtina <mu7zic2soul@gmail.com>
2018-05-04 12:39:55 -0700

    created a requset/add with axios

--- script.js
+++ script.js
@@ -1,24 +1,51 @@
-const BASE_URL = 'http://api.reactprototypes.com';
-const API_KEY = '?key=testuser1234';
+const BASE_URL = "http://api.reactprototypes.com";
+const API_KEY = "?key=c118demouser";
 
-axios.get(`${BASE_URL}/todos${API_KEY}`).then(resp => {
-    const { todos } = resp.data;
-    const table = $('table tbody');
+const response = axios.get(`${BASE_URL}/todos${API_KEY}`).then(response => {
+	console.log("Reponse: ", response);
 
-    addToDom(todos, table);
+	const listElements = response.data.todos.map((item, index) => {
+		const tr = document.createElement("tr");
+		const number = document.createElement("td");
+		const title = document.createElement("td");
+		const completed = document.createElement("td");
+
+		number.innerText = index + 1;
+		title.innerText = item.title;
+		completed.innerText = item.complete;
+
+		tr.appendChild(number);
+		tr.appendChild(title);
+		tr.appendChild(completed);
+
+		document.querySelector(".table tbody").appendChild(tr);
+	});
 });
 
-const addToDom = (list, container) => {
-    const tableRows = list.map((item, index) => {
-
-        const tableData = [
-            $(`<td>${index + 1}</td>`),
-            $(`<td>${item.title}</td>`),
-            item.complete ? $(`<td class="text-success">Yes</td>`) : $(`<td class="text-danger">No</td>`)
-        ];
-
-        return $('<tr>').append(tableData);
-    });
-
-    container.append(tableRows);
+const todoItem = {
+	title: "Christna's todo item",
+	details: "Go Running!"
 };
+
+axios
+	.post(`${BASE_URL}/todos${API_KEY}`, todoItem)
+	.then(response => {
+		console.log("Add Item Response: ", response);
+	})
+	.catch(err => {
+		try {
+			console.log("ERROR: ", err.response.data.error);
+
+			console.log(err.message);
+
+			const newError = new Error("This is the error message");
+
+			console.log("New Error: ", newError.message);
+
+			throw newError;
+
+			alert(err.response.data.error);
+		} catch (error) {
+			alert(error.message);
+		}
+	});
